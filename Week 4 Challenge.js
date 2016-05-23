d89368562e3109e2d11d85faed021b9f153b2070
Michele Morison <mmorison@umail.iu.edu>
2016-05-23 17:57:02 -0400

    Add files via upload

---
+++ Week 4 Challenge.js
@@ -0,0 +1,8 @@
+div one = document.createElement("div");
+planet.className = "planet";
+ planet.style.backgroundColor = #66ccff;
+ document.body.append("planet");
+ 
+div moon = document.createElement('div');
+ div two.className = "moon";
+ div one.body.append(planet);
